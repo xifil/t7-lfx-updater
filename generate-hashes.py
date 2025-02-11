b89f8a2b4ed886401a4cce0d26346af32b95a32d
xifil <iamspaceyt@gmail.com>
2025-02-11 18:22:02 +0000

    relocate files ('/' -> '/boiii/')

--- generate-hashes.py
+++ generate-hashes.py
@@ -3,6 +3,7 @@ import json
 import os
 
 boiii_json = []
+boiii_folder = "./boiii/"
 
 def get_file_hash(file_path):
 	sha1_hash = hashlib.sha1()
@@ -11,16 +12,10 @@ def get_file_hash(file_path):
 			sha1_hash.update(chunk)
 	return sha1_hash.hexdigest()
 
-for dirpath, _, filenames in os.walk("."):
+for dirpath, _, filenames in os.walk(boiii_folder):
 	for filename in filenames:
 		full_path = os.path.join(dirpath, filename)
-		relative_path = os.path.relpath(full_path, ".").replace(os.sep, '/')
-		if relative_path.endswith(".py") and not "/" in relative_path:
-			continue
-		if relative_path.startswith(".git"):
-			continue
-		if relative_path == "boiii.json":
-			continue
+		relative_path = os.path.relpath(full_path, boiii_folder).replace(os.sep, '/')
 		boiii_json.append([ relative_path, os.path.getsize(full_path), get_file_hash(full_path).upper() ])
 
 with open("boiii.json", "w") as json_file:
